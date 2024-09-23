43d2c5215ebf134ecd04c51d2b22d313a56607d3
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2024-09-22 23:13:46 -0500

    BOOKS_ADMINISTRATOR-develop refactor: Se modifican las longitudes máximas de los campos en el modelo libro para el correcto funcionamiento de inserción de los datos correspondientes al inventario de la biblioteca.

--- libros/models.py
+++ libros/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 class Libro(models.Model):
-    ISBN = models.CharField(max_length=20, null=True, blank=True)
-    CodigodeBarras = models.CharField(max_length=100, null=True, blank=True)
-    NoTopografico = models.CharField(max_length=100, null=True, blank=True)
+    ISBN = models.CharField(max_length=255, null=True, blank=True)
+    CodigodeBarras = models.CharField(max_length=255, null=True, blank=True)
+    NoTopografico = models.CharField(max_length=255, null=True, blank=True)
     Titulo = models.CharField(max_length=255, null=True, blank=True)
-    Autor = models.CharField(max_length=200, null=True, blank=True)
-    Ciudad = models.CharField(max_length=100, null=True, blank=True)
-    Editorial = models.CharField(max_length=200, null=True, blank=True)
-    Fecha = models.CharField(max_length=50, null=True, blank=True)
-    Edicion = models.CharField(max_length=50, null=True, blank=True)
-    Pagsovols = models.CharField(max_length=20, null=True, blank=True)
-    Ejemplares = models.CharField(max_length=20, null=True, blank=True)
-    Genero = models.CharField(max_length=50, null=True, blank=True)
-    Ubicacion = models.CharField(max_length=50, null=True, blank=True)
+    Autor = models.CharField(max_length=255, null=True, blank=True)
+    Ciudad = models.CharField(max_length=255, null=True, blank=True)
+    Editorial = models.CharField(max_length=255, null=True, blank=True)
+    Fecha = models.CharField(max_length=255, null=True, blank=True)
+    Edicion = models.CharField(max_length=255, null=True, blank=True)
+    Pagsovols = models.CharField(max_length=255, null=True, blank=True)
+    Ejemplares = models.CharField(max_length=255, null=True, blank=True)
+    Genero = models.CharField(max_length=255, null=True, blank=True)
+    Ubicacion = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.Titulo
