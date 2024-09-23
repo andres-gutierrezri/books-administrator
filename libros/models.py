c0e57f55d7b28f99377f676d5b715360dccba94f
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2024-09-22 22:50:12 -0500

    BOOKS_ADMINISTRATOR-develop refactor: Se modifican los tipos de campo en el modelo de libro para el correcto funcionamiento de inserción de los datos correspondientes al inventario de la biblioteca.

--- libros/models.py
+++ libros/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 class Libro(models.Model):
-    ISBN = models.CharField(max_length=13)
-    CodigodeBarras = models.CharField(max_length=100)
-    NoTopografico = models.CharField(max_length=100)
-    Titulo = models.CharField(max_length=200)
-    Autor = models.CharField(max_length=200)
-    Ciudad = models.CharField(max_length=100)
-    Editorial = models.CharField(max_length=100)
-    Fecha = models.DateField()
-    Edicion = models.CharField(max_length=50)
-    Pagsovols = models.IntegerField()
-    Ejemplares = models.IntegerField()
-    Genero = models.CharField(max_length=100)
-    Ubicacion = models.CharField(max_length=100)
+    ISBN = models.CharField(max_length=20, null=True, blank=True)
+    CodigodeBarras = models.CharField(max_length=100, null=True, blank=True)
+    NoTopografico = models.CharField(max_length=100, null=True, blank=True)
+    Titulo = models.CharField(max_length=255, null=True, blank=True)
+    Autor = models.CharField(max_length=200, null=True, blank=True)
+    Ciudad = models.CharField(max_length=100, null=True, blank=True)
+    Editorial = models.CharField(max_length=100, null=True, blank=True)
+    Fecha = models.CharField(max_length=50, null=True, blank=True)
+    Edicion = models.CharField(max_length=50, null=True, blank=True)
+    Pagsovols = models.CharField(max_length=20, null=True, blank=True)
+    Ejemplares = models.CharField(max_length=20, null=True, blank=True)
+    Genero = models.CharField(max_length=50, null=True, blank=True)
+    Ubicacion = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.Titulo
