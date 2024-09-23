a388a90ad45b3edcf2b8dba72cf4d6043843af39
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2024-09-22 23:03:02 -0500

    BOOKS_ADMINISTRATOR-develop refactor: Se modifica la longitud del tipo de campo editorial en el modelo libro para el correcto funcionamiento de inserción de los datos correspondientes al inventario de la biblioteca.

--- libros/models.py
+++ libros/models.py
@@ -8,7 +8,7 @@ class Libro(models.Model):
     Titulo = models.CharField(max_length=255, null=True, blank=True)
     Autor = models.CharField(max_length=200, null=True, blank=True)
     Ciudad = models.CharField(max_length=100, null=True, blank=True)
-    Editorial = models.CharField(max_length=100, null=True, blank=True)
+    Editorial = models.CharField(max_length=200, null=True, blank=True)
     Fecha = models.CharField(max_length=50, null=True, blank=True)
     Edicion = models.CharField(max_length=50, null=True, blank=True)
     Pagsovols = models.CharField(max_length=20, null=True, blank=True)
